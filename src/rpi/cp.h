793703e1d28bc61f757d88e291225a9c82965493
Ramiro Polla <ramiro.polla@gmail.com>
2017-10-11 21:19:51 +0200

    rpi: remove old raspberry pi code
    It's in git's history if we ever need it.

--- src/rpi/cp.h
+++
@@ -1,5 +0,0 @@
-// Copyright: Ramiro Polla
-// License: WTFPL
-
-// Copies txt file <from> to <to>.
-int copy_text_file(const char *from, const char *to);
--- src/rpi/lp.h
+++
@@ -1,5 +0,0 @@
-// Copyright: Ramiro Polla
-// License: WTFPL
-
-// Print <txt> file to line printer <lp>, repeating each line <n> times.
-int print(const char *txt, const char *lp, int n);
